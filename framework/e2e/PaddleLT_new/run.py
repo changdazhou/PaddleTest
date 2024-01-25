4a4b4ec3f54a04f4032a1d3a1f3ee1913e3baa53
Zeref996 <825276847@qq.com>
2024-01-25 20:03:42 +0800

    add plt env, test=model

--- framework/e2e/PaddleLT_new/run.py
+++ framework/e2e/PaddleLT_new/run.py
@@ -16,19 +16,17 @@ class Run(object):
     最终执行接口
     """
 
-    def __init__(self, layer_dir, testing):
+    def __init__(self):
         """
-
-        :param layer_dir: 所有layer.yml文件夹路径
-        :param testing: 单个testing.yml文件路径
+        init
         """
         # 获取所有layer.yml文件路径
-        self.layer_dir = layer_dir
+        self.layer_dir = os.env.get("CASE_DIR")
         self.py_list = CaseSelect(self.layer_dir).get_py_list(base_path=self.layer_dir)
 
-        self.testing = testing
+        self.testing = os.env.get("TESTING")
 
-        self.py_cmd = os.environ.get("PYTHON")
+        self.py_cmd = os.environ.get("python_ver")
         self.report_dir = os.path.join(os.getcwd(), "report")
 
     def _test_run(self):
@@ -36,12 +34,6 @@ class Run(object):
         for py_file in self.py_list:
             title = py_file.replace(".py", "").replace("/", "^").replace(".", "^")
             if platform.system() == "Windows":
-                # os.system(
-                #     "{}.exe -m pytest PaddleLT.py --all_dir"
-                #     "={} --yaml={} --case={} --testing={} --alluredir={}".format(
-                #         self.py_cmd, self.layer_dir, yaml, case, self.testing, self.report_dir
-                #     )
-                # )
                 pass
             else:
                 os.system(
@@ -51,29 +43,7 @@ class Run(object):
                     )
                 )
 
-            # for case in YamlLoader(yml=yaml).get_all_case_name():
-            #     last_dir = os.path.basename(self.layer_dir)
-            #     base_dir = self.layer_dir.replace(last_dir, "")
-            #     title = (
-            #         yaml.replace(base_dir, "").replace(".yml", ".{}".format(case)).replace("/", "^").replace(".", "^")
-            #     )
-            #     if platform.system() == "Windows":
-            #         os.system(
-            #             "{}.exe -m pytest PaddleLT.py --all_dir"
-            #             "={} --yaml={} --case={} --testing={} --alluredir={}".format(
-            #                 self.py_cmd, self.layer_dir, yaml, case, self.testing, self.report_dir
-            #             )
-            #         )
-            #     else:
-            #         os.system(
-            #             "cp -r PaddleLT.py {}.py && "
-            #             "{} -m pytest {}.py --all_dir={} --yaml={} --case={} --testing={} --alluredir={}".format(
-            #                 title, self.py_cmd, title, self.layer_dir, yaml, case, self.testing, self.report_dir
-            #             )
-            #         )
-
 
 if __name__ == "__main__":
-    test_yml = "yaml/demo_new_testing.yml"
-    tes = Run(layer_dir="diy/layer/demo_case", testing=test_yml)
+    tes = Run()
     tes._test_run()
